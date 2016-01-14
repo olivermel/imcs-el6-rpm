d1f4e584a02e43ac62b7979670d8348ff8c3edaf
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-01-14 15:05:34 -0500

    removed yum entries from bootstrap file

--- bootstrap-imcs.sh
+++ bootstrap-imcs.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install java-1.7.0*
-sudo yum -y install http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-2.noarch.rpm
-sudo yum -y install postgresql94*
-sudo yum -y install tree
-sudo yum -y install vim
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
