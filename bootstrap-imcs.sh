17d0dca54951a117626468351ac423acaa142d65
Melville Ezeogu <Melville.Ezeogu@cfpb.gov>
2016-01-14 10:45:03 -0500

    imcs files from mac

--- bootstrap-imcs.sh
+++ bootstrap-imcs.sh
@@ -3,13 +3,14 @@
 sudo yum -y groupinstall 'Development Tools'
 sudo yum -y install java-1.7.0*
 sudo yum -y install http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-2.noarch.rpm
+sudo yum -y install postgresql94*
 sudo yum -y install tree
 sudo yum -y install vim
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-if [ "$SCRIPTPATH" = "/tmp" ] ; then
+if [ "$SCRIPTPATH" = "/home" ] ; then
        SCRIPTPATH=/vagrant
    fi
   
